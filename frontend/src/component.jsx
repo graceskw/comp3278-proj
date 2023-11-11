8e56ae2551e23175deeb60aa80b96ef1544bb1bd
So Ki Wai Grace <graceskw@connect.hku.hk>
2023-11-12 01:22:25 +0800

    refactor: (frontend) change tooltip title

--- frontend/src/component.jsx
+++ frontend/src/component.jsx
@@ -51,7 +51,7 @@ function ResponsiveAppBar() {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+            <Tooltip title="Profile">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 {/* later maybe let user change profile icon */}
                 {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
